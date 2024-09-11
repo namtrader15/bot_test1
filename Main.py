637e4e3a0bf7a7f9fd23907bee013f1821040575
namtrader15 <namntn158@gmail.com>
2024-09-12 00:47:22 +0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -242,4 +242,5 @@ if __name__ == "__main__":
     trading_thread.start()
 
     # Chạy Flask server song song với bot giao dịch
-    app.run(host='0.0.0.0', port=8080)
+    # app.run(host='0.0.0.0', port=8080)
+      app.run()
